6b2d3673b223a9b4b3dffe7fd59ec0d56e578c6b
rocky <rocky@cpan.org>
2015-10-31 13:55:15 -0400

    Get ready for 2.0.0

--- lib/Devel/Trepan/Disassemble.pm
+++ lib/Devel/Trepan/Disassemble.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 # Copyright (C) 2012-2014 Rocky Bernstein <rocky@cpan.org>
 package Devel::Trepan::Disassemble;
-our $VERSION='1.99_01';
+our $VERSION='2.0.0';
 "All of the real action is in Devel::Trepan::CmdProcessor::Command::Disassemble.pm";
 __END__
 
